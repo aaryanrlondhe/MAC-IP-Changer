6788498f385875acec0075076eab5af47f70be40
UNIVERSAL CODER <63962758+aaryanrlondhe@users.noreply.github.com>
2021-09-13 21:20:39 +0530

    Update mac_ip_changer.sh

--- mac_ip_changer.sh
+++ mac_ip_changer.sh
@@ -4,4 +4,5 @@ read interface
 ifconfig $interface down
 macchanger -r $interface
 ifconfig $interface 192.168.0.3 netmask 255.255.255.0
+echo "New IP Address: 192.168.0.3 " 
 ifconfig $interface up
